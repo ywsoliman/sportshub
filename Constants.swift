84559faec10e5d49678d8b98001fcde65eee68c2
Youssef Waleed <ywsoliman@gmail.com>
2024-05-10 22:30:40 +0300

    Initial commit

---
+++ Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  SportsHub
+//
+//  Created by Youssef Waleed on 10/05/2024.
+//
+
+import Foundation
+
+struct Constants {
+    
+    private init() {}
+    
+    static let API_KEY = "463e81a5fab411a1723707c80e9aaec0c16bdfbf90536258ea9448bf4a838721"
+    static let BASE_URL = "https://apiv2.allsportsapi.com/"
+    
+}
